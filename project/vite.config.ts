2821aaa236c4873f44bcd68072f8f7b3729ca7f8
jabezs2005 <jabsudha2005@gmail.com>
2025-07-02 20:49:32 +0530

    fix

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/javex_industry_web', // ✅ moved to top-level
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
